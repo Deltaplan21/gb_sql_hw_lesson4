036df4e1ce8b29e23f703878cb9eba695d9a4e30
Deltaplan21 <52403609+Deltaplan21@users.noreply.github.com>
2019-08-12 17:04:27 +0300

    Add files via upload

---
+++ shop 4-1.sql
@@ -0,0 +1,20 @@
+/*Подсчитайте средний возраст пользователей в таблице users*/
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at varchar(50),
+  updated_at varchar(50) 
+) COMMENT = 'Покупатели';
+
+INSERT INTO users (name, birthday_at, created_at, updated_at) VALUES
+  ('Геннадий', '1990-10-05', "20.10.2017 8:10", "20.10.2017 8:10"),
+  ('Наталья', '1984-11-12', "20.10.2017 8:10", "20.10.2017 8:10"),
+  ('Александр', '1985-05-20', "20.10.2017 8:10", "20.10.2017 8:10"),
+  ('Сергей', '1988-02-14', "20.10.2017 8:10", "20.10.2017 8:10"),
+  ('Иван', '1998-01-12', "20.10.2017 8:10", "20.10.2017 8:10"),
+  ('Мария', '1992-08-29', "20.10.2017 8:10", "20.10.2017 8:10");
+
+select AVG((TIMESTAMPDIFF(year, birthday_at, now()))) as 'Средний возраст пользователей, лет' from users
